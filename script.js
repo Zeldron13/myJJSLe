d959079cdc91c85df87b40d42c7bf7e466a966c5
Zeka <zeka2111@bk.ru>
2022-01-07 22:14:17 +0300

    Простые числа

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 "use struct";
 
+let num = 10;
 
-let currentUserName = prompt("Представтесь", ["Евгений"]);
-let isTrue = confirm(`Вас зовут ${currentUserName}?`);
-alert(isTrue);
+out:
+for(let i = 2; i <= num; i++){
+    for(let j = 2; j < i; j++){
+        if(i % j == 0) continue out;
+    }
+    alert(`Простое ${i}`);
+}
+// let n = 10;
+
+// nextPrime:
+// for (let i = 2; i <= n; i++) { // Для всех i...
+
+//   for (let j = 2; j < i; j++) { // проверить, делится ли число..
+//     if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+//   }
+
+//   alert( i ); // простое число
+// }
